00e5643b3083d82f3ffda00ed21970b2c8abc6b5
Patrik Björklund <patrik@cognit.se>
2016-10-28 10:10:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# scripts"  git init git add README.md git commit -m "first commit" git remote add origin git@github.com:pbjorklund/scripts.git git push -u origin master
---
+++ removeappbloat.ps1
@@ -0,0 +1,2 @@
+﻿"3dbuilder", "windowsalarms", "windowscommunicationsapps", "windowscamera", "zunemusic", "windowsmaps", "zunevideo", "onenote", "people", "photos", "soundrecorder", "xboxapp", "Microsoft.MicrosoftStickyNotes", "Microsoft.BingWeather", "Microsoft.BingTranslator", "Solitaire", "Skype", "Messaging",  | Foreach-Object { Get-AppxPackage *$_* | Remove-AppxPackage }
+
